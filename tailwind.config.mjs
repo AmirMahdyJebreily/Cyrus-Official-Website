75fe653d3c8fa0749f32f32fd697a32bd8e1afcd
[ Taha. Dostifam ] <mr.tahadostifam@gmail.com>
2025-01-11 14:47:52 +0330

    tailwindcss, rippleui, fonts and logo init

---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "Vazirmatn", "sans-serif"], 
+        jersey15: ["Jersey15"], 
+      },
+    },
+  },
+  plugins: [require("rippleui")],
+  /** @type {import('rippleui').Config} */
+  rippleui: {
+    themes: [
+      {
+        themeName: "light",
+        colorScheme: "light",
+        colors: {
+          primary: "#8c533b",
+          secondary: "#d1d1d1",
+          backgroundPrimary: "#ffffff",
+        },
+      },
+      {
+        themeName: "dark",
+        colorScheme: "dark",
+        colors: {
+          primary: "#8c533b",
+          secondary: "#171717",
+          backgroundPrimary: "#000000",
+        },
+      },
+    ],
+  },
+};
